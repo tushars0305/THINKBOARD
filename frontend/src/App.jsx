eee8bc22942aa9c508b661949258b9a6bf85b608
Tushar Singh <tusharsinghinu@gmail.com>
2025-07-15 20:36:00 +0530

    created home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,8 @@ const App = () => {
 
       </Routes>
       
-    </div>
+    </div> 
+    
   )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
-    <div>
-      HOME
+    <div className="min-h-screen">
+      <Navbar />
+      
     </div>
   )
 }
