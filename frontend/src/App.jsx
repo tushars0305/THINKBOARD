bd45def41f4de90b68ed1d6aa10d66daade9387b
Tushar Singh <tusharsinghinu@gmail.com>
2025-07-25 20:14:13 +0530

    fix the error while deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router";
-
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import NoteDetailPage from "./pages/NoteDetailPage";
+import NoteDetailPage from './pages/NoteDetailpage';
 
 const App = () => {
   return (
     <div className="relative h-full w-full">
-<div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>      <Routes>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
